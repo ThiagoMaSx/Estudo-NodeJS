4d5d5a71faa94f0b9e8f54a5f923b2a8d32c8f4a
ThiagoMaSx <thiagowstf@gmail.com>
2026-09-25 17:01:18 -0300

    Banco de dados em memória

--- db-memory.js
+++ db-memory.js
@@ -1,3 +1,23 @@
-export class databaseMemory {
+import { randomUUIDv7 } from "crypto"
 
-}
+export class databaseMemory {
+    #videos = new Map()
+    
+    list() {
+        return this.#videos.values()
+    }
+
+    create(video) {
+        const videoId = randomUUIDv7
+
+        this.#videos.set(videoId, video)
+    }
+
+    update(id,video) {
+        this.#videos.set(id,video)
+    }
+
+    delete(id,video) {
+        this.#videos.delete(id)
+    }
+}   
--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 import { databaseMemory } from "./db-memory";
+import fastify from "fastify";
 
 //Criando servidor com fastify
-import fastify from "fastify";
 
 const server = fastify()
 
+const database = new databaseMemory ()
+
+
 server.post('/videos', () => {
-    return "Hello world"
+    database.create({
+        title: 'Video about birds',
+        description: 'yes, just birds',
+        duration: 180
+
+    })
 })
 
 server.get('/videos', () => {
